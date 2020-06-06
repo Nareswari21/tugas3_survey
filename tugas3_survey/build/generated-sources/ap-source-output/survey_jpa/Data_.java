cabf6b87efcc046e5f52eff5f2a198c46fa98298
Nareswari21 <nareswari219@gmail.com>
2020-06-06 21:20:48 +0700

    first

--- tugas3_survey/build/generated-sources/ap-source-output/survey_jpa/Data_.java
+++ tugas3_survey/build/generated-sources/ap-source-output/survey_jpa/Data_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-06T18:44:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-06T21:15:11")
 @StaticMetamodel(Data.class)
 public class Data_ { 
 
